5de99e962852e371964fc78319a4144f3333314a
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-03-26 11:12:44 -0300

    Adding responsive design

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,25 @@
-let projectsContainer = document.querySelector('[data-projectsContainer]');
-let projectsBtn = document.querySelector('[data-btn]');
+// Getting the elements
+let projectsContainer = document.querySelector("[data-projectsContainer]");
+let projectsBtn = document.querySelector("[data-btn]");
 
+// Adding event listener to the button
 let buttonState = 0;
-projectsBtn.addEventListener('click', function(){
-    if (buttonState === 0) {
-        projectsBtn.style.marginBottom = 0;
-        projectsContainer.classList.remove('hidden');
-        setTimeout(function(){
-            projectsContainer.classList.add('shown');
-            projectsBtn.innerHTML = 'Show Less';
-        }, 200);
-        buttonState = 1;
-        console.log(buttonState)
-    } else if (buttonState === 1) {
-        projectsContainer.classList.remove('shown');
-        setTimeout(function(){
-            projectsContainer.classList.add('hidden');
-            projectsBtn.style.marginBottom = '1rem';
-            projectsBtn.innerHTML = 'Show More';
-        }, 400);
-        buttonState = 0;
-        console.log(buttonState)
-    }
+projectsBtn.addEventListener("click", function () {
+  if (buttonState === 0) {
+    projectsBtn.style.marginBottom = 0;
+    projectsContainer.classList.remove("hidden");
+    setTimeout(function () {
+      projectsContainer.classList.add("shown");
+      projectsBtn.innerHTML = "Show Less";
+    }, 200);
+    buttonState = 1;
+  } else if (buttonState === 1) {
+    projectsContainer.classList.remove("shown");
+    setTimeout(function () {
+      projectsContainer.classList.add("hidden");
+      projectsBtn.style.marginBottom = "1rem";
+      projectsBtn.innerHTML = "Show More";
+    }, 400);
+    buttonState = 0;
+  }
 });
